84f06c46dcb2ad89b886239b797072c85e9fe728
Kahnu Charan Swain <kahnuc.swain@nettantra.net>
2022-11-18 08:14:17 +0530

    Db Updated

--- utils/connectMongo.js
+++ utils/connectMongo.js
@@ -1,4 +1,27 @@
 import mongoose from "mongoose";
 
-const connect=async()=>mongoose.connect(process.env.DB_URL)
+const connect = () => {
+
+    //check if it is already connected to database
+    if(mongoose.connections[0].readyState){
+        console.log('Connected to datbase');
+        return;
+    }
+
+    mongoose.connect(process.env.KANHU_DB);
+
+    mongoose.connection.on('connected', ()=>{
+        console.log('connected successfully');
+    })
+
+    mongoose.connection.on('error', (err)=>{
+        console.log('connection failed', err);
+    })
+
+
+
+
+}
+
+
 export default connect;
