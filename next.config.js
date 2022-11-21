47b4299fb6e790f022090687d5bd5c1429e9c786
sandip parida <lit.sandip.parida282@gmail.com>
2022-11-21 23:23:19 +0530

    add pwa

--- next.config.js
+++ next.config.js
@@ -1,14 +1,8 @@
 /** @type {import('next').NextConfig} */
-
-
-
 const withPWA = require('next-pwa')({
   dest: 'public',
-  register: false,
-  skipWaiting: true,
 })
 
 module.exports = withPWA({
-  // next.js config
-  reactStrictMode: true,
+  // config
 })
