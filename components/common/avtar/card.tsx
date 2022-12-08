479bedf73c37186f06990f38aec157d1326d5b73
sandip parida <lit.sandip.parida282@gmail.com>
2022-12-08 16:22:02 +0530

    merge

--- components/common/avtar/card.tsx
+++ components/common/avtar/card.tsx
@@ -4,20 +4,11 @@ import { useSession, signIn, signOut } from "next-auth/react"
 export default function card() {
   return (
 <>
- 
-
- 
-  <div className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
-    <div className="py-1" role="none">
-      {/* <a href="#" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-0">Account settings</a>
-      <a href="#" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-1">Support</a>
-      <a href="#" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-2">License</a> */}
-      <form method="POST" action="#" role="none">
-        <button type="submit" onClick={() =>signOut()} className="text-gray-700 block w-full px-4 py-2 text-left text-sm" role="menuitem" tabindex="-1" id="menu-item-3">Sign out</button>
-      </form>
+  <div className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" >
+    <div className="py-1">
+        <button type="submit" onClick={() =>signOut()} className="text-gray-700 block w-full px-4 py-2 text-left text-sm" >Sign out</button>
     </div>
   </div>
-
 </>
 
   )
